e6be5bcbd0f011b1c0d221de0efff34e43cfe810
Liam Zdenek <liamzdenek@gmail.com>
2013-04-27 19:34:47 -0500

    Adding TODO file

---
+++ TODO.php
@@ -0,0 +1,19 @@
+h1. TODO List
+
+not in any sort of priority. lines with question marks are uncertain.
+
+- Singleton
+    - Databases
+        - Redis
+        - MongoDB
+        - Memcached?
+        - CouchDB?
+        - Riak?
+        - Cassandra?
+        - Hypertable?
+    - ZeroMQ
+    - Logging
+    - Some sort of input validation? extra and not part of the core m2php-e?
+- Internationalisation
+- Unit Tests?
+
